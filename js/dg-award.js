17fd662cfb5f5c8bb12e4cb2c80694d5d326836c
Jackliu <jackliulovelt@outlook.com>
2017-08-24 00:15:16 +0800

    完成dgAwardUtil

--- js/dg-award.js
+++ js/dg-award.js
@@ -10,7 +10,6 @@ var dgAward = window.dgAward  || {
             'wechat':{content:"wxp://f2f0FQyMGuEt3K-YvCtxx0Vu8A-XS3X92uE7",name:"微信",desc:"微信打赏",className:"wechat",logo:""}
         }
     },
-    
     init:function () {
         if(!document.body){
             setTimeout(dgAward.init,0)
@@ -31,7 +30,61 @@ var dgAwardUtil = {
         for (var k in params){
             paramsAll += k + "=" + params[k];
         }
-
         return (baseUrl+'?'+paramsAll).slice(0,-1)
+    },
+//    生成三个不等随机数
+//count为随机数个数，返回数组
+    genRanNum : function (lowLimit,upLimit,count) {
+        // noinspection JSAnnotator
+        var ranNum,ranArr = [];
+        if(count){
+            ranNum  = Math.round((upLimit - lowLimit - count -1) * Math.random());
+            for(var i=0,len=count; i<len; i++){
+                ranArr.push(ranNum+i)
+            }
+        }else{
+            ranNum =  Math.round((upLimit - lowLimit) * Math.random());
+            ranArr.push(ranNum)
+        }
+        return ranArr;
+    },
+    /***在目标元素target最后创建新元素。
+     * 新元素标签名为tagName,属性由param键值对决定
+     */
+    createElement	: function(param, tagName, target) {
+        var element = document.createElement(tagName || "div");
+        for (var key in param) {
+            key == "style" ? (element[key].cssText = param[key]) : (element[key] = param[key])
+        }
+        return (target || document.body).appendChild(element);
+    },/***
+     * 根据className获得元素
+     */
+    getElement:	function(param) {
+        if (param.charAt(0) === "#") {
+            if (document.getElementById(param.slice(1))) {
+                return document.getElementById(param.slice(1))
+            }
+        }
+        if (param.indexOf(".") === 0) {
+            if (document.getElementsByClassName(param.slice(1))) {
+                return document.getElementsByClassName(param.slice(1))[0];
+            }
+
+        }
+
+        if (document.getElementsByTagName(param)[0]) {
+            return document.getElementsByTagName(param)[0];
+        }
+        return false;
+    },
+    onClick : function (element,func) {
+        if(getElement(element)){
+            getElement(element).addEventListener("click",function () {
+                func();
+            })
+        }
     }
+
+
 };
