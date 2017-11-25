28b0f9a076f0d5917c02f441fcdf68932962ddcd
Jackliu <jackliulovelt@outlook.com>
2017-11-25 21:06:44 +0800

    use qrcode.js

--- js/dgAward.js
+++ js/dgAward.js
@@ -3,64 +3,64 @@ var dgAward = {
         // imagePrefix : "",
         // cssPrefix : "",
         // cssUrl:"",
-        appendName:window.myConfig.appendName || ".containner",
-        logoBaseUrl:"http://ovc3ypwtu.bkt.clouddn.com",
-        logoImage:window.myConfig.logoImage || "http://ovc3ypwtu.bkt.clouddn.com/android-chrome-96x96.png",
-        showMes:window.myConfig.showMes|| '"如果这篇文章帮助到你，你可能想给我买杯咖啡 :)"',
-        hideMes:window.myConfig.hideMes|| '"您的赞赏，是我创作的最大鼓励。"',
-        baseUrl:window.myConfig.baseUrl||"http://qr.liantu.com/api.php",
-        listConfig:window.myConfig.listConfig||[
-    {configName:'alipay',content:"HTTPS://QR.ALIPAY.COM/FKX04775JJLPLHW03GV321",name:"支付宝",desc:"支付宝打赏",className:"alipay",logo:""},
-    {configName:'wechat',content:"wxp://f2f0FQyMGuEt3K-YvCtxx0Vu8A-XS3X92uE7",name:"微信",desc:"微信打赏",className:"wechat",logo:""}
+        appendName: window.myConfig.appendName || ".containner",
+        logoBaseUrl: "http://ovc3ypwtu.bkt.clouddn.com",
+        logoImage: window.myConfig.logoImage || "http://ovc3ypwtu.bkt.clouddn.com/android-chrome-96x96.png",
+        showMes: window.myConfig.showMes || '"如果这篇文章帮助到你，你可能想给我买杯咖啡 :)"',
+        awardMes: window.myConfig.awardMes || '（扫描或长按二维码识别打赏）',
+        hideMes: window.myConfig.hideMes || '"您的赞赏，是我创作的最大鼓励。"',
+        baseUrl: window.myConfig.baseUrl || "http://qr.liantu.com/api.php",
+        listConfig: window.myConfig.listConfig || [{
+                configName: 'alipay',
+                content: "HTTPS://QR.ALIPAY.COM/FKX04775JJLPLHW03GV321",
+                name: "支付宝",
+                desc: "支付宝打赏",
+                className: "alipay",
+                logo: ""
+            },
+            {
+                configName: 'wechat',
+                content: "wxp://f2f0FQyMGuEt3K-YvCtxx0Vu8A-XS3X92uE7",
+                name: "微信",
+                desc: "微信打赏",
+                className: "wechat",
+                logo: ""
+            }
         ]
     },
     wrapper: null,
-    myRewards:null,
-    awardPeopleLogo:null,
-    iconLi:null,
-    awardShow:null,
-    awardHide:null,
-    qrcodeWrapper:null,
-    eventListen: function(){
-        dgAwardUtil.onClick("#awardLogo",function () {
+    myRewards: null,
+    awardPeopleLogo: null,
+    iconLi: null,
+    awardShow: null,
+    awardHide: null,
+    qrcodeWrapper: null,
+    eventListen: function () {
+        dgAwardUtil.onClick("#awardLogo", function () {
             dgAwardUtil.getElement("#awardHide").style.display = "block"
         });
-        dgAwardUtil.onClick("#closeSwitch",function () {
+        dgAwardUtil.onClick("#closeSwitch", function () {
             dgAwardUtil.getElement("#awardHide").style.display = "none"
         })
     },
-    qrUrl: function(){
-      var config = dgAward.myConfig.listConfig;
-      var urlArr = [];
-        for(var i=0,len=config.length; i<len; i++){
-          conf = config[i]
-          baseUrl = dgAward.myConfig.baseUrl;
-          params = {
-              text : conf.content,
-              w:150
-            }
-            urlArr.push(dgAwardUtil.generateUrl(params,baseUrl))
-        }
-        return urlArr;
-    },
     generateCss: function () {
         dgAwardUtil.createElement({
-            href:dgAward.myConfig.logoBaseUrl+"/style.css",
+            href: dgAward.myConfig.logoBaseUrl + "/style.css",
             type: "text/css",
             rel: "stylesheet"
-        },"link",dgAwardUtil.getElement("head"))
+        }, "link", dgAwardUtil.getElement("head"))
     },
     generateMyRewards: function () {
         this.myRewards = dgAwardUtil.createElement({
             className: "award-show"
-        },"div", this.wrapper)
+        }, "div", this.wrapper)
 
     },
     generateWrapper: function () {
         this.wrapper = dgAwardUtil.createElement({
             className: "award-wrapper",
-            id:         "awardWrapper"
-        },"",dgAwardUtil.getElement(dgAward.myConfig.appendName));
+            id: "awardWrapper"
+        }, "", dgAwardUtil.getElement(dgAward.myConfig.appendName));
         this.generateAwardShow();
         this.generateAwardHide();
         // this();
@@ -70,131 +70,135 @@ var dgAward = {
         var awardHide = dgAwardUtil.createElement({
             className: "award-hide",
             id: "awardHide"
-        },"div",this.wrapper);
+        }, "div", this.wrapper);
         var closeSwitch = (function () {
             var closeSwitch = dgAwardUtil.createElement({
-                className:"close-switch",
-                id:"closeSwitch"
-            },"",awardHide)
+                className: "close-switch",
+                id: "closeSwitch"
+            }, "", awardHide)
             var svgIcon = dgAwardUtil.createElement({
-                className:"svg-icon svg-icon-del",
-                style:"background-image:url('"+dgAward.myConfig.logoBaseUrl+"/icon-del.svg')"
-            },"i",closeSwitch)
+                className: "svg-icon svg-icon-del",
+                style: "background-image:url('" + dgAward.myConfig.logoBaseUrl + "/icon-del.svg')"
+            }, "i", closeSwitch)
         })();
         var authorLogo = (function () {
             var authorLogo = dgAwardUtil.createElement({
                 className: "author-logo"
-            },"",awardHide);
+            }, "", awardHide);
             // console.log(dgAward.myConfig.logoImage)
             dgAwardUtil.createElement({
                 className: "author-logo-img",
-                style:"background: url('"+dgAward.myConfig.logoImage+"');background-size:cover;"
-            },"",authorLogo)
+                style: "background: url('" + dgAward.myConfig.logoImage + "');background-size:cover;"
+            }, "", authorLogo)
         })()
         var awardSays = dgAwardUtil.createElement({
             className: "award-says",
-            innerText:dgAward.myConfig.hideMes
-        },"",awardHide)
+            innerText: dgAward.myConfig.hideMes
+        }, "", awardHide)
+        dgAwardUtil.createElement({
+            className: "award-says",
+            innerText: dgAward.myConfig.awardMes
+        }, "", awardHide)
         var qrcodeWrapper = (function () {
             var qrcodeWrapper = dgAwardUtil.createElement({
-                className:"qrcode-wrapper"
-            },"",awardHide);
-            // var qrcodeAli = dgAwardUtil.createElement({
-            //     className: "qrcode qrcodeAli"
-            // },"",qrcodeWrapper)
-            // var qrcodeWc = dgAwardUtil.createElement({
-            //     className: "qrcode qrcodeWc"
-            // },"",qrcodeWrapper)
-            var img = (function () {
-                for(var i=0,len=dgAward.qrUrl().length; i<len; i++){
+                className: "qrcode-wrapper"
+            }, "", awardHide);
+            var qrCanvas = (function () {
+                for (var i = 0, len = dgAward.myConfig.listConfig.length; i < len; i++) {
                     var qrcode = dgAwardUtil.createElement({
-                        className:"qrcode qrcode-"+dgAward.myConfig.listConfig[i].className
-                    },"",qrcodeWrapper);
-                    var img = dgAwardUtil.createElement({
-                        src:dgAward.qrUrl()[i]
-                    },"img",qrcode);
+                        className: "qrcode qrcode-" + dgAward.myConfig.listConfig[i].className
+                    }, "", qrcodeWrapper);
+                    dgAwardUtil.genQrcode(qrcode,dgAward.myConfig.listConfig[i].content)
                     dgAwardUtil.createElement({
                         innerText: dgAward.myConfig.listConfig[i].name
-                    },"h3",qrcode)
+                    }, "h3", qrcode)
                 }
             })();
         })();
 
-        
+
     },
-    generateAwardShow:function(){
-        var num = dgAwardUtil.genRanNum(3,17)
+    generateAwardShow: function () {
+        var num = dgAwardUtil.genRanNum(3, 17)
 
         this.awardShow = dgAwardUtil.createElement({
             className: "award-show"
-        },"div", this.wrapper);
+        }, "div", this.wrapper);
         dgAwardUtil.createElement({
-            className:"award-title",
-            innerText:dgAward.myConfig.showMes
-        },"h3",this.awardShow);
+            className: "award-title",
+            innerText: dgAward.myConfig.showMes
+        }, "h3", this.awardShow);
         dgAwardUtil.createElement({
-            className:"award-logo",
-            id:"awardLogo",
+            className: "award-logo",
+            id: "awardLogo",
             innerText: "赏"
-        },"div",this.awardShow);
+        }, "div", this.awardShow);
         dgAwardUtil.createElement({
-            className:"award-people",
-            id:"awardPeople",
-            innerText: num+"人打赏"
-        },"div",this.awardShow);
+            className: "award-people",
+            id: "awardPeople",
+            innerText: num + "人打赏"
+        }, "div", this.awardShow);
         this.generateAwardPeopleLogo();
     },
-    generateAwardPeopleLogo : function(){
-        var imgNum = dgAwardUtil.genRanNum(1,10,3);
+    generateAwardPeopleLogo: function () {
+        var imgNum = dgAwardUtil.genRanNum(1, 10, 3);
 
-        this.awardPeopleLogo =dgAwardUtil.createElement({
+        this.awardPeopleLogo = dgAwardUtil.createElement({
             className: "award-people-logo"
-        },"",this.awardShow)
+        }, "", this.awardShow)
         this.ul = dgAwardUtil.createElement({
             className: "award-ul"
-        },"ul",this.awardPeopleLogo);
-        for (var i = 1;i<=3;i++){
+        }, "ul", this.awardPeopleLogo);
+        for (var i = 1; i <= 3; i++) {
             this.iconLi = dgAwardUtil.createElement({
-                className:"icon-li "+"icon-"+i,
-                style:"background:url('"+dgAward.myConfig.logoBaseUrl+"/logo-"+imgNum[i-1]+".jpg');background-size:contain;",
-                id: "icon-"+i
-            },"li",this.ul)
+                className: "icon-li " + "icon-" + i,
+                style: "background:url('" + dgAward.myConfig.logoBaseUrl + "/logo-" + imgNum[i - 1] + ".jpg');background-size:contain;",
+                id: "icon-" + i
+            }, "li", this.ul)
         }
     },
-    init:function () {
-        if(!document.body){
-            setTimeout(dgAward.init,0)
-        }else{
-            dgAward.generateCss();
+    init: function () {
+        if (!document.body) {
+            setTimeout(dgAward.init, 0)
+        } else {
+            // dgAward.generateCss();
             dgAward.generateWrapper();
-            dgAward.qrUrl();
             dgAward.eventListen()
         }
     }
 
 };
 var dgAwardUtil = {
-    //http://qr.liantu.com/api.php?w=150&text=HTTPS://QR.ALIPAY.COM/FKX04775JJLPLHW03GV321
-    // params ={}
-    generateUrl:function (params,baseUrl) {
+    generateUrl: function (params, baseUrl) {
         var paramsAll = '';
-        for (var k in params){
+        for (var k in params) {
             paramsAll += k + "=" + params[k] + '&';
         }
-        return (baseUrl+'?'+paramsAll).slice(0,-1)
+        return (baseUrl + '?' + paramsAll).slice(0, -1)
     },
-//    生成三个不等随机数
-//count为随机数个数，返回数组
-    genRanNum : function (lowLimit,upLimit,count) {
+    // 新建一个二维码
+    genQrcode: function (ele, text, width, colorDark, colorLight, correctLevel) {
+        var qrcode = new QRCode(ele, {
+            text: text,
+            width: width || 128,
+            height: width || 128,
+            colorDark: colorDark || "#000000",
+            colorLight: colorLight || "#ffffff",
+            correctLevel: correctLevel || QRCode.CorrectLevel.H
+        });
+    },
+    //    生成三个不等随机数
+    //count为随机数个数，返回数组
+    genRanNum: function (lowLimit, upLimit, count) {
         // noinspection JSAnnotator
-        var ranNum,ranArr = [];
-        if(count){
-            ranNum  = Math.round((upLimit  - lowLimit-count +1) * Math.random() + lowLimit);
-            for(var i=0,len=count; i<len; i++){
-                ranArr.push(ranNum+i)
+        var ranNum, ranArr = [];
+        if (count) {
+            ranNum = Math.round((upLimit - lowLimit - count + 1) * Math.random() + lowLimit);
+            for (var i = 0, len = count; i < len; i++) {
+                ranArr.push(ranNum + i)
             }
-        }else{
-            ranNum =  Math.round((upLimit - lowLimit ) * Math.random() + lowLimit);
+        } else {
+            ranNum = Math.round((upLimit - lowLimit) * Math.random() + lowLimit);
             ranArr.push(ranNum)
         }
         return ranArr;
@@ -202,16 +206,17 @@ var dgAwardUtil = {
     /***在目标元素target最后创建新元素。
      * 新元素标签名为tagName,属性由param键值对决定
      */
-    createElement	: function(param, tagName, target) {
+    createElement: function (param, tagName, target) {
         var element = document.createElement(tagName || "div");
         for (var key in param) {
             key == "style" ? (element[key].cssText = param[key]) : (element[key] = param[key])
         }
         return (target || document.body).appendChild(element);
-    },/***
+    },
+    /***
      * 根据className获得元素
      */
-    getElement:	function(param) {
+    getElement: function (param) {
         if (param.charAt(0) === "#") {
             if (document.getElementById(param.slice(1))) {
                 return document.getElementById(param.slice(1))
@@ -229,9 +234,9 @@ var dgAwardUtil = {
         }
         return false;
     },
-    onClick : function (element,func) {
-        if(dgAwardUtil.getElement(element)){
-            dgAwardUtil.getElement(element).addEventListener("click",function () {
+    onClick: function (element, func) {
+        if (dgAwardUtil.getElement(element)) {
+            dgAwardUtil.getElement(element).addEventListener("click", function () {
                 func();
             })
         }
